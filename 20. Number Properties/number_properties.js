f128a9c3ba85ec8f8fefa158a06df484c7767aff
Zed0825 <www.ragaszedrick25@gmail.com>
2023-12-22 21:25:32 +0800

    Update Number properties syntax

--- 20. Number Properties/number_properties.js
+++ 20. Number Properties/number_properties.js
@@ -12,6 +12,52 @@ function maxValue(){
     document.getElementById('demo2').innerHTML = "maxValue: " + x;
 };
 
+// Number.MIN_VALUE is a constant representing the lowest possible number in JavaScript.
+function minValue(){
+    let x = Number.MIN_VALUE;
+
+    document.getElementById('demo3').innerHTML = "minValue: " + x;
+};
+
+// Number.MAX_SAFE_INTEGER represents the maximum safe integer in JavaScript. Number.MAX_SAFE_INTEGER is (253 - 1).
+function maxSafeInteger(){
+    let x = Number.MAX_SAFE_INTEGER;
+
+    document.getElementById('demo4').innerHTML = "maxSafeInteger: " + x;
+};
+
+// Number.MIN_SAFE_INTEGER represents the minimum safe integer in JavaScript. Number.MIN_SAFE_INTEGER is -(253 - 1).
+function minSafeInteger(){
+    let x = Number.MIN_SAFE_INTEGER;
+
+    document.getElementById('demo5').innerHTML = "minSafeInteger: " + x;
+};
+
+// POSITIVE_INFINITY is returned on overflow:
+function infinity(){
+    let x = 1 / 0;
+
+    document.getElementById('demo6').innerHTML = "infinity: " + x;
+};
+
+function negativeInfinity(){
+    let x = -1 / 0;
+
+    document.getElementById('demo7').innerHTML = "negativeInfinity: " + x;
+};
+
+function Not_a_Number(){
+    let x = 100 / "Apple";
+
+    document.getElementById('demo8').innerHTML = "Not a Number: " + x;
+};
+
 window.onload =
 epsilon()
-maxValue();
+maxValue()
+minValue()
+maxSafeInteger()
+minSafeInteger()
+infinity()
+negativeInfinity()
+Not_a_Number();
