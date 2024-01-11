3c3d45562bd8352b37cfbd516c4c89c5a3e2919f
Zed0825 <www.ragaszedrick25@gmail.com>
2024-01-11 19:10:59 +0800

    Update API Syntax

--- API/api.js
+++ API/api.js
@@ -36,11 +36,88 @@ function constraintsValidationAPI(){
         };
     });
 }
+// Constraint Validation DOM Properties
+
+// Validity
+// validationMessage
+// willValidate
+
+// Validity Properties
+
+// customError
+// patternMismatch
+// rangeOverflow
+// rangeUnderflow
+// stepMismatch
+// toLong
+// typeMismatch
+// valueMissing
+// valid
 
 // Range Overflow
+function rangeOverflow(){
+    const input = document.getElementById('demo3-input');
+    const button = document.getElementById('demo3-button');
 
+    button.addEventListener("click", function(){
+        if(input.validity.rangeOverflow){
+            document.getElementById('demo3-p').innerHTML = "Value too large.";
+        };
+    });
+};
+
+// Range Underflow
+function rangeOverflow(){
+    const input = document.getElementById('demo4-input');
+    const button = document.getElementById('demo4-button');
+
+    button.addEventListener("click", function(){
+        if(input.validity.rangeUnderflow){
+            document.getElementById('demo4-p').innerHTML = "Value too small.";
+        };
+    });
+};
+
+// historyBack()
+function historyBack(){
+    const back = document.getElementById('back-button-back');
+    back.addEventListener('click', function(){
+        console.log('Back');
+        window.history.back();
+    });
+};
+// historyGo() - go back to specific history count
+function historyGo(){
+    const back = document.getElementById('back-button-go');
+    back.addEventListener('click', function(){
+        console.log('Go');
+        window.history.go(-2); // go back 2 pages
+    });
+};
+
+// web storage api
+
+// setItem
+function setItem(){
+    const text = document.getElementById('demo5-input');
+    const submit = document.getElementById('demo5-submit');
+
+    submit.addEventListener('click', function(){
+        localStorage.setItem('name', text.value);
+    });
+};
+
+// getItem
+function getItem(){
+    console.log(localStorage.getItem('name'));
+};
 
 window.onload = 
 introToAPI()
 worldTimeApi()
-constraintsValidationAPI();
+constraintsValidationAPI()
+rangeOverflow()
+historyBack()
+historyGo()
+setItem()
+getItem();
